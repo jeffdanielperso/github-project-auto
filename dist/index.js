8296f85af87215a2735e136bbb7bb7bca465590c
Jeff <jeff.daniel@ziwo.io>
2021-05-24 00:45:45 +0200

    fix

--- dist/index.js
+++ dist/index.js
@@ -65,6 +65,25 @@ exports.runLabelsAction = runLabelsAction;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -77,23 +96,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.runProjectAction = void 0;
 const debug_1 = __nccwpck_require__(371);
+const core = __importStar(__nccwpck_require__(186));
 function runProjectAction(octokit, actionData) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            const projectName = core.getInput('project');
+            const columnName = core.getInput('column');
+            if (!projectName || !columnName)
+                return;
             // Get projects
-            const orgProjects = yield octokit.rest.projects.listForOrg({
-                org: actionData.owner
-            });
+            // const orgProjects = await octokit.rest.projects.listForOrg({
+            //   org: actionData.owner
+            // });
             const repoProjects = yield octokit.rest.projects.listForRepo({
                 owner: actionData.owner,
                 repo: actionData.repo
             });
-            const userProjects = yield octokit.rest.projects.listForUser({
-                username: actionData.owner
-            });
-            debug_1.debugLog(`org ${JSON.stringify(orgProjects)}`);
+            // const userProjects = await octokit.rest.projects.listForUser({
+            //   username: actionData.owner
+            // });
+            // debugLog(`org ${JSON.stringify(orgProjects)}`);
             debug_1.debugLog(`org ${JSON.stringify(repoProjects)}`);
-            debug_1.debugLog(`org ${JSON.stringify(userProjects)}`);
+            // debugLog(`org ${JSON.stringify(userProjects)}`);
         }
         catch (error) {
             debug_1.debugLog(`[Error/project.ts] ${error}`);
--- src/actions/project.ts
+++ src/actions/project.ts
@@ -1,27 +1,33 @@
 import {GithubActionData} from '../triggers/github';
 import {GitHub} from '@actions/github/lib/utils';
 import {debugLog} from '../debug/debug';
+import * as core from '@actions/core';
 
 export async function runProjectAction(
   octokit: InstanceType<typeof GitHub>,
   actionData: GithubActionData
 ): Promise<void> {
   try {
+    const projectName = core.getInput('project');
+    const columnName = core.getInput('column');
+
+    if (!projectName || !columnName) return;
+
     // Get projects
-    const orgProjects = await octokit.rest.projects.listForOrg({
-      org: actionData.owner
-    });
+    // const orgProjects = await octokit.rest.projects.listForOrg({
+    //   org: actionData.owner
+    // });
     const repoProjects = await octokit.rest.projects.listForRepo({
       owner: actionData.owner,
       repo: actionData.repo
     });
-    const userProjects = await octokit.rest.projects.listForUser({
-      username: actionData.owner
-    });
+    // const userProjects = await octokit.rest.projects.listForUser({
+    //   username: actionData.owner
+    // });
 
-    debugLog(`org ${JSON.stringify(orgProjects)}`);
+    // debugLog(`org ${JSON.stringify(orgProjects)}`);
     debugLog(`org ${JSON.stringify(repoProjects)}`);
-    debugLog(`org ${JSON.stringify(userProjects)}`);
+    // debugLog(`org ${JSON.stringify(userProjects)}`);
   } catch (error) {
     debugLog(`[Error/project.ts] ${error}`);
     throw error;
