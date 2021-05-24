7e78377b795cfdeccbf9aaf503c0b4efdc999e97
Jeff <jeff.daniel@ziwo.io>
2021-05-24 22:05:15 +0200

    dd

--- dist/index.js
+++ dist/index.js
@@ -205,9 +205,10 @@ function findMatchingCard(octokit, columns, issue) {
 function createCard(octokit, column, issue) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            const issueNumber = issue.number;
             const request = {
                 column_id: column.id,
-                content_id: issue.number,
+                content_id: issueNumber,
                 content_type: 'Issue',
                 note: `note ${column.id} - ${issue.number} - ${issue.id}`
             };
